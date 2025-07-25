43fc4e4617a1d7b33e645ead03b262ba3625c8d1
psalminfo <psalm4all@gmail.com>
2025-07-25 11:12:51 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,8 @@
 // js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCt7kk_oCBuftEgHeFhoZU2cPrPROhC6JE",
   authDomain: "bkh-assessments.firebaseapp.com",
@@ -11,5 +12,6 @@ const firebaseConfig = {
   appId: "1:86179403579:web:5295877bb92d0a18f3091e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
