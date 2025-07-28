8b7ee64891e81442412d71a192e5d1b657689243
psalminfo <psalm4all@gmail.com>
2025-07-29 00:33:52 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,11 @@
+// firebaseConfig.js
 const firebaseConfig = {
   apiKey: "AIzaSyD1lJhsWMMs_qerLBSzk7wKhjLyI_11RJg",
   authDomain: "bloomingkidsassessment.firebaseapp.com",
   projectId: "bloomingkidsassessment",
   storageBucket: "bloomingkidsassessment.appspot.com",
   messagingSenderId: "238975054977",
-  appId: "1:238975054977:web:87c70b4db044998a204980",
+  appId: "1:238975054977:web:87c70b4db044998a204980"
 };
 
 // Initialize Firebase globally
