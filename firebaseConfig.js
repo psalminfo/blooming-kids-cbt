d838cd10724d723d59c5cd143db92947968af67e
psalminfo <psalm4all@gmail.com>
2025-07-29 09:22:39 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,21 @@
 // firebaseConfig.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD1lJhsWMMs_qerLBSzk7wKhjLyI_11RJg",
-  authDomain: "bloomingkidsassessment.firebaseapp.com",
-  projectId: "bloomingkidsassessment",
-  storageBucket: "bloomingkidsassessment.appspot.com",
-  messagingSenderId: "238975054977",
-  appId: "1:238975054977:web:87c70b4db044998a204980"
+  apiKey: 'AIzaSyD1lJhsWMMs_qerLBSzk7wKhjLyI_11RJg',
+  authDomain: 'bloomingkidsassessment.firebaseapp.com',
+  projectId: 'bloomingkidsassessment',
+  storageBucket: 'bloomingkidsassessment.appspot.com',
+  messagingSenderId: '238975054977',
+  appId: '1:238975054977:web:87c70b4db044998a204980'
 };
 
-// Initialize Firebase globally
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
