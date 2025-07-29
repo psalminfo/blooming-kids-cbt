c68c5eb1657086ddb3b0a01a2bc6e161a510bbc7
psalminfo <psalm4all@gmail.com>
2025-07-29 18:49:50 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,14 @@
-// firebaseConfig.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyD1lJhsWMMs_qerLBSzk7wKhjLyI_11RJg',
-  authDomain: 'bloomingkidsassessment.firebaseapp.com',
-  projectId: 'bloomingkidsassessment',
-  storageBucket: 'bloomingkidsassessment.appspot.com',
-  messagingSenderId: '238975054977',
-  appId: '1:238975054977:web:87c70b4db044998a204980'
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { app, auth, db, storage };
+<!-- Add this in your HTML <head> (index.html, subject-select.html, etc) -->
+<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-auth-compat.js"></script>
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyD1lJhsWMMs_qerLBSzk7wKhjLyI_11RJg",
+    authDomain: "bloomingkidsassessment.firebaseapp.com",
+    projectId: "bloomingkidsassessment",
+    storageBucket: "bloomingkidsassessment.appspot.com",
+    messagingSenderId: "238975054977",
+    appId: "1:238975054977:web:87c70b4db044998a204980"
+  };
+  firebase.initializeApp(firebaseConfig);
+</script>
