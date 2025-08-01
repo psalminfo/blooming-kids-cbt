3dbb96be28deabc4887892f3d5307c0414ba9721
psalminfo <psalm4all@gmail.com>
2025-08-01 01:23:50 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,16 @@
-// firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyBpwxvEoeuT8e6F5vGmDc1VkVfWTUdxavY",
-  authDomain: "blooming-kids-house.firebaseapp.com",
-  projectId: "blooming-kids-house",
-  storageBucket: "blooming-kids-house.appspot.com",
-  messagingSenderId: "739684305208",
-  appId: "1:739684305208:web:ee1cc9e998b37e1f002f84"
+const firebaseConfig = {
+  apiKey: "AIzaSyD1lJhsWMMs_qerLBSzk7wKhjLyI_11RJg",
+  authDomain: "bloomingkidsassessment.firebaseapp.com",
+  projectId: "bloomingkidsassessment",
+  storageBucket: "bloomingkidsassessment.appspot.com",
+  messagingSenderId: "238975054977",
+  appId: "1:238975054977:web:87c70b4db044998a204980"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export { collection, addDoc };
+export const storage = getStorage(app);
